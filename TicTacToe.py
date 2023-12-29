2d4957552c9deccaf985a637a70bcd1dd09b220b
Franklin <franklinjiachen@gmail.com>
2023-12-28 21:10:29 -0800

    Create Minimax

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,3 +1,15 @@
+MOVE_MAPPING = {
+    0: 'Top Left', 1: 'Top Center', 2: 'Top Right',
+    3: 'Middle Left', 4: 'Middle Center', 5: 'Middle Right',
+    6: 'Bottom Left', 7: 'Bottom Center', 8: 'Bottom Right'
+}
+
+MINIMAX_VALUE_MAPPING = {
+    1: 'X winning',
+    0: 'a draw',
+    -1: 'O winning'
+}
+
 class TicTacToe:
     def __init__(self, board_str: str = None) -> None:
         """
@@ -61,6 +73,73 @@ class TicTacToe:
         """
         return ' ' not in self.board
 
-game = TicTacToe('XOXOOXOX ')
+    def minimax(self, depth: int, maximizing_player: bool) -> int:
+        """
+        Implement the minimax algorithm to find the optimal move. 'X' is the maximizing player.
 
-game.display_board()
+        Parameters:
+        - depth: Current depth in the game tree (recursive calls).
+        - maximizing_player: True if the current player is trying to maximize the score ('X'), False otherwise ('O').
+
+        Returns:
+        - The minimax value for the current state of the board.
+        """
+
+        # Base cases
+        if self.check_winner('X'):
+            return 1
+        elif self.check_winner('O'):
+            return -1
+        elif self.check_draw():
+            return 0
+
+        if maximizing_player:
+            max_eval = float('-inf')
+            for i in range(9):
+                if self.board[i] == ' ':
+                    self.board[i] = 'X'
+                    eval = self.minimax(depth + 1, False)
+                    self.board[i] = ' '
+                    max_eval = max(max_eval, eval)
+            return max_eval
+        else:
+            min_eval = float('inf')
+            for i in range(9):
+                if self.board[i] == ' ':
+                    self.board[i] = 'O'
+                    eval = self.minimax(depth + 1, True)
+                    self.board[i] = ' '
+                    min_eval = min(min_eval, eval)
+            return min_eval
+
+    def find_best_move(self) -> int:
+        """
+        Find the best move for the 'X' player using the minimax algorithm.
+        In case of multiple equally optimal moves, the tiebreaker is the lower index.
+
+        Returns:
+        - The index of the best move on the board.
+        - The minimax value of the best move given.
+        """
+        best_val = float('-inf')
+        best_move = -1
+
+        for i in range(9):
+            if self.board[i] == ' ':
+                self.board[i] = 'X'
+                move_val = self.minimax(0, False)
+                self.board[i] = ' '
+
+                if move_val > best_val:
+                    best_val = move_val
+                    best_move = i
+
+        return best_move, best_val
+
+if __name__ == "__main__":
+    game = TicTacToe('O   X   O')
+    game.display_board()
+    best_move, best_val = game.find_best_move()
+
+    print(f"A best move for 'X' is: {MOVE_MAPPING[best_move]}.")
+    print(f"With optimal play, this position leads to {MINIMAX_VALUE_MAPPING[best_val]}")
